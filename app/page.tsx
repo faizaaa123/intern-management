b1919cc102dfbb53e91ee883a9304658d8e475fa
Gels04 <gelcira2004@outlook.com>
2023-08-12 08:01:14 +0100

    created public and protected components, which renders conditionally.

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,25 @@
 "use client"
-import Image from 'next/image'
-import styles from './page.module.css'
-import {useState} from "react";
+// pages/_app.tsx
 
-export default function Home() {
-  const [status, setStatus] = useState("");
-  const handleClick = () => {
-    // User swiped down, set status to 'working from home'
-    setStatus('working from home');
-  };
+import { useEffect, useState } from 'react';
+import keycloak from '../keycloakConfig';
+import Public from '../components/Public';
+import Protected from '../components/Protected';
 
-  const handleDoubleClick = () => {
-    // User swiped up, set status to 'in office'
-    setStatus('in office');
-  };
+const MyApp: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+
+  useEffect(() => {
+    keycloak.init({ onLoad: 'login-required' }).then((isAuthenticated: boolean | ((prevState: boolean) => boolean)) => {
+      setIsAuthenticated(isAuthenticated);
+    });
+  }, []);
 
   return (
     <div>
-      <h1>Manual Check-in System</h1>
-      <div
-        style={{
-          width: '200px',
-          height: '200px',
-          background: '#007bff',
-          color: '#fff',
-          fontSize: '20px',
-          fontWeight: 'bold',
-          cursor: 'pointer',
-          userSelect: 'none',
-          border: "1px black solid"
-        }}
-        onDoubleClick={handleDoubleClick}
-        onClick={handleClick}
-      >
-        Tap to Check-in "Working from Home" or Double Tap to Check-in "In Office"
-      </div>
-      {status && <p>You are currently checked in as "{status}"</p>}
+      {isAuthenticated ? <Protected /> : <Public />}
     </div>
   );
 };
+
+export default MyApp;
--- server/routes/login.js
+++ server/routes/login.js
@@ -2,6 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 const User = require("../../db/userModel");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+
+dotenv.config()
+
+const {
+    AUTH0_SECRET,
+    AUTH0_CLIENT_SECRET,
+    AUTH0_CLIENT_ID,
+    AUTH0_ISSUER_BASE_URL,
+    BASE_URL
+  } = process.env
+
+
 
 
 async function checkExistingUser(user) {
@@ -15,7 +29,11 @@ async function checkExistingUser(user) {
 router.get("/", async (req, res) => {
     res.json(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out')
 
-    if(req.oidc.isAuthenticated()) {
+    if(!req.oidc.isAuthenticated()) {
+
+        res.status(500).json({message: "Login failed."})
+
+    } else {
 
         const options = {
             method: "POST",
@@ -26,8 +44,10 @@ router.get("/", async (req, res) => {
         }
         try {
             const response = await fetch("http://localhost:5005/users", options)
+            const idToken = req.oidc.idToken
+            //const verifiedToken = jwt.verify(idToken, AUTH0_CLIENT_SECRET, {alorgithms: ['RS256']})
             const data = await response.json()
-            console.log(data);
+            console.log(data, "idToken: ", idToken,);
         } catch (error) {
             console.log(error);
         }
